6b13c8f5b9c51b6831bbeebc1150c2418950a32c
DuncanMoyo <duncanfmoyo@gmail.com>
2024-03-17 11:34:09 +0200

    Designed and Implemented the Header Section

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="min-h-screen w-full flex justify-center items-center">{children}</div>;
+};
+
+export default Layout;
