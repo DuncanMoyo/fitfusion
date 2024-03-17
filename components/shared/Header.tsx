ec59aa9a42c3f192177030602a154e26f5d98076
DuncanMoyo <duncanfmoyo@gmail.com>
2024-03-17 14:38:28 +0200

    Added Mobile Nav and full Clerk Functionality

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,25 +1,43 @@
-import { SignedOut } from "@clerk/nextjs"
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "../ui/button"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeaderLinks from "./HeaderLinks";
+import MobileMenu from "./MobileMenu";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full flex items-center justify-between">
-        <Link href='/' className="w-36">
-          <Image src='/assets/fitfusion.svg' width={50} height={50} alt="logo"/>
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/fitfusion.svg"
+            width={50}
+            height={50}
+            alt="logo"
+          />
         </Link>
+
+        <SignedIn>
+          <nav className="w-full max-w-xs md:flex md:flex-between hidden ">
+            <HeaderLinks />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
-           <SignedOut>
-             <Button asChild className="rounded-full" size='lg'>
-              <Link href='sign-in'>Login</Link>
-             </Button>
-           </SignedOut>
+          <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+            <MobileMenu />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const headerLinks = [
+  {
+    displayName: "Home",
+    path: "/",
+  },
+  {
+    displayName: "Create Event",
+    path: "/events/create",
+  },
+  {
+    displayName: "My Profile",
+    path: "/profile",
+  },
+];
