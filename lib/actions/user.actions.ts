29e23b94be92aeb1367499dd12b66581240d86e5
DuncanMoyo <duncanfmoyo@gmail.com>
2024-03-18 21:45:21 +0200

    Updated the user paramters and added more async functions to user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,18 +1,67 @@
 "use server";
 
-import { CreateUserParams } from "@/types";
+import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError } from "../utils";
 import { establishDatabaseConnection } from "@/mongodb";
 import User from "@/mongodb/models/user.model";
+import { revalidatePath } from "next/cache";
 
-export const createUser = async (user: CreateUserParams) => {
+export async function createUser(user: CreateUserParams) {
   try {
     await establishDatabaseConnection();
 
     const newUser = await User.create(user);
-
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
   }
-};
+}
+
+export async function getUserById(userId: string) {
+  try {
+    await establishDatabaseConnection();
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await establishDatabaseConnection();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new Error("User update failed");
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function deleteUser(clerkId: string) {
+  try {
+    await establishDatabaseConnection();
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error("User not found");
+    }
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
+  }
+}
