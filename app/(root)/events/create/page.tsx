7a72e0b66831045baf1524cf468304e03780567a
DuncanMoyo <duncanfmoyo@gmail.com>
2024-03-21 04:01:17 +0200

    Added update/create events page

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,23 @@
+import { FitnessEventForm } from "@/components/shared";
+import { auth } from "@clerk/nextjs";
+
+const CreateEvent = () => {
+  const {sessionClaims} = auth();
+
+  const userId = sessionClaims?.userId as string;
+  return (
+    <>
+      <section>
+        <h3 className="text-center sm:text-left font-bold text-[28px] leading-[36px] md:text-[36px] md:leading-[44px] max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full">
+          Create Event
+        </h3>
+      </section>
+
+      <div className="my-8 max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full">
+        <FitnessEventForm userId={userId} type='Create'/>
+      </div>
+    </>
+  );
+};
+
+export default CreateEvent;
--- types/index.ts
+++ types/index.ts
@@ -14,3 +14,23 @@ export type UpdateUserParams = {
   username: string
   photo: string
 }
+
+// FITNESS EVENT PARAMETERS
+// ====== EVENT PARAMS
+export type CreateEventParams = {
+  userId: string
+  event: {
+    title: string
+    description: string
+    location: string
+    imageUrl: string
+    startDateTime: Date
+    endDateTime: Date
+    categoryId: string
+    price: string
+    isFree: boolean
+    url: string
+  }
+  path: string
+}
+
